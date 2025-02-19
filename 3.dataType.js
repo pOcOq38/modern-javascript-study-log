ec77ca6bde3059b80694661b7ca136d875ab5bef
pOcOq38 <cylee0322@gmail.com>
2025-02-19 20:38:28 +0900

    add naming, dataType

---
+++ 3.dataType.js
@@ -0,0 +1,20 @@
+let integer = 123;
+let negative = -123;
+let double = 1.23;
+
+let binary = 0b1111011;
+let octal = 0o173;
+let hex = 0x7b;
+
+console.log(0/123);
+console.log(123/0); // Infinity
+console.log(123/-0); // -Infinity
+console.log(123/'test'); // not a number
+
+//String
+let string = 'hello';
+string = "'hello'";
+
+string = 'hello \n Elle \t\t hehe\\';
+console.log(strin, '11');
+
